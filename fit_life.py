dcf0569faa6985b6352a1d1222219a7bae626203
Georgii Portnov <georgii_portnov@mail.ru>
2026-06-16 23:22:24 +0400

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -1,9 +1,11 @@
 # Проект FitLife - MVP версия 1.0
 import sys
+
 sys.stdout.reconfigure(encoding='utf-8')
 
+
 def main():
-   
+    """Основная функция программы FitLife."""
     # 1. Знакомство
     user_name = input("Введите Ваше имя: ")
     user_age = input("Сколько вам лет? ")
